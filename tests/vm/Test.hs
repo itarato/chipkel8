31e8fdb225f4136655cfdb5f6a71b89f5cd82a41
Peter Arato <it.arato@gmail.com>
2024-02-14 08:45:12 -0500

    tests

--- tests/vm/Test.hs
+++ tests/vm/Test.hs
@@ -16,13 +16,22 @@ tests = testGroup "Tests" [unitTests]
 unitTests =
   testGroup
     "VM tests"
-    [ testVMInitState
+    [ testVMInitState,
+      testVMPCInc
     ]
 
 testVMInitState :: TestTree
-testVMInitState = testCase "PC is 0x200" (assertPc *> assertStack *> assertRegs)
+testVMInitState = testCase "VM init state" (assertPc *> assertStack *> assertRegs)
   where
     vm = initVM []
     assertPc = assertEqual "PC is 0x200" 0x200 $ pc vm
     assertStack = assertEqual "Stack size" 16 $ length $ stack vm
     assertRegs = assertEqual "Regs size" 16 $ length $ regs vm
+
+testVMPCInc :: TestTree
+testVMPCInc = testCase "PC inc" (assertDefault *> assertIncreased)
+  where
+    vm = initVM []
+    assertDefault = assertEqual "PC is 200" 0x200 $ pc vm
+    vm' = withPCInc vm
+    assertIncreased = assertEqual "PC is 202" 0x202 $ pc vm'
