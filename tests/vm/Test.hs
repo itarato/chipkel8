eb196d559b3d79875ca1c0600013efa7dab160a8
Peter Arato <it.arato@gmail.com>
2024-02-15 07:18:39 -0500

    tests

--- tests/vm/Test.hs
+++ tests/vm/Test.hs
@@ -16,8 +16,9 @@ unitTests =
   testGroup
     "VM tests"
     [ testVMInitState,
-      testVMPCInc,
-      testVMReturnFromStack
+      testVMWithPCInc,
+      testVMWithReturnFromStack,
+      testVMWithCall
     ]
 
 testVMInitState :: TestTree
@@ -28,16 +29,16 @@ testVMInitState = testCase "VM init state" (assertPc *> assertStack *> assertReg
     assertStack = assertEqual "Stack size" 16 $ length $ stack vm
     assertRegs = assertEqual "Regs size" 16 $ length $ regs vm
 
-testVMPCInc :: TestTree
-testVMPCInc = testCase "PC inc" (assertDefault *> assertIncreased)
+testVMWithPCInc :: TestTree
+testVMWithPCInc = testCase "PC inc" (assertDefault *> assertIncreased)
   where
     vm = initVM []
     assertDefault = assertEqual "PC is 200" 0x200 $ pc vm
     vm' = withPCInc vm
     assertIncreased = assertEqual "PC is 202" 0x202 $ pc vm'
 
-testVMReturnFromStack :: TestTree
-testVMReturnFromStack = testCase "Return from stack" (assertPC *> assertSP)
+testVMWithReturnFromStack :: TestTree
+testVMWithReturnFromStack = testCase "Return from stack" (assertPC *> assertSP)
   where
     vm = initVM []
     _stack = stack vm
@@ -45,3 +46,12 @@ testVMReturnFromStack = testCase "Return from stack" (assertPC *> assertSP)
     vm'' = withReturnFromStack vm'
     assertPC = assertEqual "PC is 123" 123 $ pc vm''
     assertSP = assertEqual "SP is 0XF" 0xF $ sp vm''
+
+testVMWithCall :: TestTree
+testVMWithCall = testCase "Call" (assertPC *> assertSP *> assertStack)
+  where
+    vm = withCall 123 $ initVM []
+    _stack = stack vm
+    assertPC = assertEqual "PC is 123" 123 $ pc vm
+    assertSP = assertEqual "SP is 0xE" 0xE $ sp vm
+    assertStack = assertEqual "Last addr is on stack" 0x200 $ (V.!) _stack 0xF
